ef3944bb5ced1d8bec7da0ebff3de09e9c9f2261
sicambria <inczegaspar@gmail.com>
2016-07-15 16:15:58 +0200

    Run this script if you get a blank page and/or gulp dev is not running.

---
+++ fix_gulp_dev.sh
@@ -0,0 +1,12 @@
+sudo gem install bundler
+sudo gem install rake
+bundle install
+
+sudo npm install -g bower
+sudo npm install -g gulp
+sudo npm install -g gulp-sass --save-dev
+
+cd angular
+sudo npm install gulp
+sudo npm update
+sudo npm install
